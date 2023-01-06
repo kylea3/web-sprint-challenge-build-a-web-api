e571ac697e5378103105001051c0099d81b80e01
Kyle Andrus <kyleandrus3@gmail.com>
2023-01-06 16:19:32 -0700

    add post response to actions-router.js, need to debug

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,6 +1,7 @@
 // add middlewares here related to actions
 // add middlewares here related to projects
 const Action = require('./actions-model')
+const Project = require('../projects/projects-model')
 
 async function validateUserId(req, res, next) {
     try {
@@ -17,17 +18,32 @@ async function validateUserId(req, res, next) {
 }
 
 function validateBody(req, res, next) {
-    const { description, name, completed } = req.body;
-    if (!description || !name|| !completed ) {
-        res.status(400).json({ "message": "Need to include a description and name field"})
+    const { description, completed, notes, project_id } = req.body;
+    if (!description || !notes|| !completed || !project_id ) {
+        res.status(400).json({ "message": "Need to include a description, completed, notes, and project_id field"})
     } else {
-        req.project = { description: description, name: name, completed: completed || false };
+        req.action = { description: description, notes: notes, completed: completed , project_id: project_id  };
         next()
     }
 }
 
+async function validateProject_Id(req, res, next) {
+        try {
+            const project = await Project.get(req.body.project_id);
+            if(!project) {
+              res.status(404).json({ message: "There is not a project with that id" })
+            } else {
+              next();
+            }
+            }
+        catch (err) {
+              next(err)
+              }
+    }
+
 
 module.exports = {
     validateUserId,
-    validateBody
+    validateBody,
+    validateProject_Id
 }
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Action = require('./actions-model')
+const { validateUserId, validateBody, validateProject_Id } = require('./actions-middlware')
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
@@ -10,6 +11,22 @@ router.get('/', (req, res, next) => {
         .catch(next)
 })
 
+router.get('/:id', validateUserId, async (req, res, next) => {
+    Action.get(req.params.id)
+        .then(action  => {
+            res.status(200).json(action)
+        })
+        .catch(next)
+})
+
+router.post('/', validateBody, validateProject_Id, (req, res, next) => {
+    console.log(req.action)
+    Action.insert(req.action)
+        .then(action => {
+            res.status(201).json(action)
+        })
+        .catch(next)
+})
 
 
 module.exports = router;
