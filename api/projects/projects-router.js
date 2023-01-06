313fe4f312c1e80fc6a97583d29074aeeeabf3f4
Kyle Andrus <kyleandrus3@gmail.com>
2023-01-06 12:43:11 -0700

    set up server and connect routers to server.js

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,35 @@
+const express = require('express')
+const Project = require('./projects-model')
+const router = express.Router();
 // Write your "projects" router here!
+router.get('/api/projects', (req, res) => {
+    Project.get()
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
+
+router.get('/api/projects/:id', (req, res) => {
+    
+})
+
+router.post('/api/projects', (req, res) => {
+    
+})
+
+router.put('/api/projects/:id', (req, res) => {
+    
+})
+
+router.delete('/api/projects:id', (req, res) => {
+    
+})
+
+router.get('/api/projects/:id/actions', (req, res) => {
+    
+})
+
+module.exports = router
