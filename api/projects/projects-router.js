f8a739fdc6fe05f6b96642e8a98151f6eb01cf86
Kyle Andrus <kyleandrus3@gmail.com>
2023-01-06 12:59:01 -0700

    add get response for specific project ids in projects-router.js

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -3,7 +3,6 @@ const Project = require('./projects-model')
 const router = express.Router();
 // Write your "projects" router here!
 router.get('/', (req, res) => {
-    console.log(Project.get())
     Project.get()
         .then(project => {
             res.status(200).json(project)
@@ -13,8 +12,19 @@ router.get('/', (req, res) => {
         })
 })
 
-router.get('/:id', (req, res) => {
-    
+router.get('/:id', async (req, res) => {
+    const id = await Project.get(req.params.id);
+    if(id) {
+    Project.get(req.params.id)
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(err => {
+            res.status(500).json({ "message": "There was an issue with the server" })
+        })
+    } else {
+        res.status(404).json({ "message": "There is no project with the given id" })
+    }
 })
 
 router.post('/', (req, res) => {
